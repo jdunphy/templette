ef0465e56c852cf0c271a90780d8e01914c23293
Jacob Dunphy <jacob.dunphy@gmail.com>
2008-07-01 19:22:20 -0700

    Moving rake tasks into a gem file.  Gets required in Rakefile now.

---
+++ lib/tasks/templette_tasks.rb
@@ -0,0 +1,33 @@
+desc "Build an HTML project from template files"
+task(:build) do
+  Templette::Generator.new.run
+end
+
+desc "Remove all generated files"
+task :clean do
+  Templette::Generator.new.clean
+end
+
+namespace :generate do
+  
+  desc "Generate empty page files based upon TEMPLATE= and NAMES="
+  task :page_yaml do
+    unless ENV['TEMPLATE'] && ENV['NAMES']
+      puts "You must set TEMPLATE AND NAMES to run this task."
+      return
+    end
+    FileGenerator.page_yaml(ENV['TEMPLATE'], ENV['NAMES'])
+  end
+  
+  desc "Generate an empty generator file with NAME=example"
+  task :helper do
+    unless ENV['NAME']
+      puts "You must enter a name for your helper."
+      return
+    end
+    FileGenerator.helper(ENV['NAME'])
+  end 
+
+end
+
+task :default => :build
