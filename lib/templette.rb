8cde8c72a86cbdd3cb0d35f46ce7bd18e873ea11
Jacob Dunphy <jacob.dunphy@gmail.com>
2008-07-01 19:23:22 -0700

    We don't need these requires.  Helpers will get required as needed by page generation.

--- lib/templette.rb
+++ lib/templette.rb
@@ -1,6 +1,3 @@
 %w{data_accessors errors generator page template}.each do |file|
   require File.dirname(__FILE__) +"/templette/#{file}"
 end
-Dir['./helpers/*.rb'].each do |file|
-  require file.gsub(/.rb$/, '')
-end
