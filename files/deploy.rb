4d335039a76bc3c3e881ce2a0150be38eaf670ce
Jacob Dunphy <jacob.dunphy@gmail.com>
2008-07-15 23:44:24 -0700

    Adding a deploy namespace to the cap tasks.

--- files/deploy.rb
+++ files/deploy.rb
@@ -12,24 +12,26 @@ task :production do
   set :deploy_path, 'public_html/path'
 end
 
-before "deploy", "package"
-after "deploy", "cleanup"
+before "deploy", "deploy:package"
+after "deploy", "deploy:cleanup"
 
-task :package do
- `rake build destination=#{package_name}`
- `tar -czvf #{package_name}.tar.gz #{package_name}`
+namespace :deploy do
+  
+  task :package do
+   `rake build destination=#{package_name}`
+   `tar -czvf #{package_name}.tar.gz #{package_name}`
+  end
+
+  desc "This is the main task"
+  task :default, :roles => [:web] do
+   upload "#{package_name}.tar.gz", "#{staging_path}#{package_name}.tar.gz"
+   run "tar -xzvf #{staging_path}#{package_name}.tar.gz"
+   run "rsync -r #{staging_path}#{package_name}/ #{deploy_path}"
+  end
+
+  task :cleanup do
+    run "rm -f #{staging_path}/#{package_name}"
+    run "rm -rf #{staging_path}#{package_name}"
+    `rm -rf #{package_name}.tar.gz`
+  end
 end
-
-desc "This is the main task"
-task :deploy, :roles => [:web] do
- upload "#{package_name}.tar.gz", "#{staging_path}#{package_name}.tar.gz"
- run "tar -xzvf #{staging_path}#{package_name}.tar.gz"
- run "rsync -r #{staging_path}#{package_name}/ #{deploy_path}"
-end
-
-task :cleanup do
-  run "rm -f #{staging_path}/#{package_name}"
-  run "rm -rf #{staging_path}#{package_name}"
-  `rm -rf #{package_name}.tar.gz`
-end
-
