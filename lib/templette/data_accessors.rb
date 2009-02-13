dc1cc982183185a17dd6b9c3133ad7bef9169c9e
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-02-12 17:57:54 -0800

    Updating the rdoc.

--- lib/templette/data_accessors.rb
+++ lib/templette/data_accessors.rb
@@ -1,37 +1,55 @@
 module Templette
   module DataAccessors
-    def attributes 
+    def attributes # :nodoc:
       @attributes ||= {}
     end
     
-    def generate_accessors(accessors = {})
+    def generate_accessors(accessors = {}) # :nodoc:
       accessors.each_pair { |k,v| generate_accessor(k, v) }
     end
         
-    def include_helpers(helpers)
+    def include_helpers(helpers) # :nodoc:
       helpers.each { |helper| add_helper(helper) }
     end
     
-    def method_missing(symbol)
+    def method_missing(symbol) # :nodoc:
       raise PageError.new(page, "No method '#{symbol}' defined in the yaml")
     end
-
-    def partial(filename)
+    
+    def partial(filename) # :nodoc:
       raise PageError.new(page, "Rendering #{filename} failed.  File not found.") unless File.exists?(filename)
       Engineer.engine_for(Engineer.determine_type(filename)).render(File.read(filename), page._binding)
     rescue RenderError => e
       raise PageError.new(page, e.message)      
     end
+
+    # Generates an image tag.  Default options {:alt => filename }
+    # 
+    # Ex. 
+    #    image_tag('ball.png', :alt => 'a red ball')
+    #    => "<img src='/images/ball.png' alt='a red ball' />"
     
     def image_tag(path, options = {})
       options = {:alt => path}.merge(options)
       "<img src='/images/#{path}' #{params_to_attributes(options)}/>"
     end
     
+    # Generates a link to a stylesheet.  Default options {:type => 'text/css'}
+    # 
+    # Ex. 
+    #    stylesheet_tag('print', :media => 'print')
+    #    => "<link href='/stylesheets/print.css' media='print' type='text/css' />"
+    
     def stylesheet_tag(path, options = {})
       options = {:type => 'text/css'}.merge(options)
       "<link href='/stylesheets/#{path}.css' #{params_to_attributes(options)}/>"
     end
+    
+    # Genrates a javascript scrip tag.
+    #
+    # Ex.
+    #     script_tag('slider')
+    #     => <script src='/javascripts/slider.js' type='text/javascript'></script>
     
     def script_tag(path)
       "<script src='/javascripts/#{path}.js' type='text/javascript'></script>"
