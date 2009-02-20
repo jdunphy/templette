a179d3ca011b8618798d8fd9f8f168954a4d335d
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-02-20 08:52:22 -0800

    Cleaning up the MethodCollector tests.

--- test/method_collector_test.rb
+++ test/method_collector_test.rb
@@ -2,34 +2,35 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class MethodCollectorTest < Test::Unit::TestCase
   
-  def test_unnested_method_collection
-    template = Templette::Template.new('foo')
-    template.stubs(:path).returns('foo.html')
-    File.stubs(:exists?).returns(true)
-    Templette::MethodCollector.any_instance.stubs(:include_helpers)
-    template.expects(:to_html).returns("<html><%= foo %> | <%= boo %> </html>")
-    mc = Templette::MethodCollector.new(template)
-    assert mc.to_hash.has_key?('foo')
-    assert mc.to_hash.has_key?('boo')
+  context "A MethodCollector" do
+    setup do
+      @template = Templette::Template.new('test')
+      @template.stubs(:path).returns('test.html')
+      File.stubs(:exists?).returns(true)
+      Templette::MethodCollector.any_instance.stubs(:include_helpers)
+    end
+    
+    should "collect unchained methods" do
+      mc = collect_methods("<html><%= foo %> | <%= boo %> </html>")
+      assert mc.to_hash.has_key?('foo')
+      assert mc.to_hash.has_key?('boo')
+    end
+    
+    should "collect chained methods" do
+      mc = collect_methods("<html><%= foo %> | <%= foo.two %> </html>")
+      assert mc.to_hash.has_key?('foo')
+      assert mc.to_hash['foo'].has_key?('two')
+    end
+    
+    should "return nil when no methods are collected" do
+      assert_nil collect_methods("<html></html>").to_hash
+    end
   end
   
-  def test_nested_method_collection
-    template = Templette::Template.new('footwo')
-    template.stubs(:path).returns('foo.html')
-    File.stubs(:exists?).returns(true)
-    Templette::MethodCollector.any_instance.stubs(:include_helpers)
-    template.expects(:to_html).returns("<html><%= foo %> | <%= foo.two %> </html>")
-    mc = Templette::MethodCollector.new(template)
-    assert mc.to_hash.has_key?('foo')
-    assert mc.to_hash['foo'].has_key?('two')
-  end
+  private 
   
-  def test_method_collector_returns_nil_for_no_methods
-    template = Templette::Template.new('fooyou')
-    template.stubs(:path).returns('foo.html')
-    File.stubs(:exists?).returns(true)
-    Templette::MethodCollector.any_instance.stubs(:include_helpers)
-    template.expects(:to_html).returns("<html></html>")
-    assert_nil Templette::MethodCollector.new(template).to_hash
-  end
+    def collect_methods(html)
+      @template.expects(:to_html).returns(html)
+      Templette::MethodCollector.new(@template)
+    end
 end
