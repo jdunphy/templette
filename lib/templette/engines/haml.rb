c560cd8958afca84e6e03283b0dba5af7cf5a641
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-03-02 14:30:11 -0800

    Stole this from Hanna.  I don't like all those icky Haml warnings.

--- lib/templette/engines/haml.rb
+++ lib/templette/engines/haml.rb
@@ -5,9 +5,21 @@ module Templette
     class Haml
     
       def render(html, the_binding)
-        ::Haml::Engine.new(html).render(the_binding)
+        with_no_warnings { ::Haml::Engine.new(html).render(the_binding) }
       end
-    
+      
+      
+      def with_no_warnings
+        save = $-w
+        $-w = false
+
+        begin
+          yield
+        ensure
+          $-w = save
+        end
+      end
+      
     end
   end
 end
